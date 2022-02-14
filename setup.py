bb7d9f61f7b074c69d6933e97954ad0685ebeb3f
Aaron Hanson <spinnelein@gmail.com>
2022-02-14 14:12:17 -0800

    .

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='covid',
+    version='',
+    packages=['covid'],
+    url='',
+    license='',
+    author='Aaron',
+    author_email='',
+    description=''
+)
